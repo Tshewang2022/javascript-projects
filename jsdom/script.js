e362a9f9287df8be28b97ff98a227deadf129d5c
Tshewang Gyaltshen <tshewanggyaltshen0@gmail.com>
2025-04-27 00:49:01 +0600

    remember small steps always add up to bigger ones

--- jsdom/script.js
+++ jsdom/script.js
@@ -2,8 +2,10 @@
 const header = document.querySelector('.header');
 const button = document.querySelector('.btn-button')
 const message = document.createElement('div');
+const navLink = document.querySelector('.nav_link');
 message.classList.add('cookie-message');
 message.innerHTML = 'we use cookie to store some data';
+
 
 // inserting inside the dom
 header.prepend(message);
@@ -12,5 +14,19 @@ header.append(message);
 // removing the element from the domPapillon, this is much more efficient and powerful
 button.addEventListener('click', () => message.remove());
 
-message.style.backgroundColor = '#000000'
+message.style.backgroundColor = '#ffffff'
 
+// event propagations=> event bubling
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav_link').addEventListener('click', function (e) {
+    // cannot use the arrow function here, because arrow function does not have this keyword
+    this.style.backgroundColor = randomColor();
+    console.log('link')
+});
+
+// dom traversing
+const h1 = document.querySelector('h1');
+console.log(h1.document.querySelectorAll('.h3'));
